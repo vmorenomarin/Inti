28bed81b15d986bbcf7da4fb9f666cf37ec31942
vmorenomamrin <vmorenomarin@gmail.com>
2020-11-20 19:03:07 -0500

    Path to ScieloRequest class file fixed

--- tests/test_scielorequest.py
+++ tests/test_scielorequest.py
@@ -1,11 +1,11 @@
 import unittest
 import json
-from inti.SCIELO.scielorequest import scielorequest
+from inti.SCIELO.scielorequest import ScieloRequest
 from pymongo import MongoClient
 
 
 class TestScieloRequest(unittest.TestCase):
-    """ Testing class. """
+    """Testing class."""
 
     def setUp(self):
         self.scielorequest = self.ScieloRequest(db='scielo', host=None)
@@ -15,7 +15,7 @@ class TestScieloRequest(unittest.TestCase):
         self.assertEqual(self.scielorequest.host, None)
 
     def test__get_collections(self):
-        """Test get_collections method.""""
+        """Test get_collections method."""
 
         collections = ''
         f = open('tests/collections.json')
@@ -28,7 +28,7 @@ class TestScieloRequest(unittest.TestCase):
                 self.assertEqual(col[key], s_col[key])
 
     def test__get_journals(self):
-        """Test get_journals method.""""
+        """Test get_journals method."""
 
         journals = ''
         f = open('tests/journals.json')
@@ -37,18 +37,18 @@ class TestScieloRequest(unittest.TestCase):
         s_jrls = self.scielorequest.get_journals()
 
         for jrl, s_jrl in zip(journals, s_jrls):
-            for key in in jrl.keys():
+            for key in jrl.keys():
                 self.assertEqual(jrl[key], s_jrl[key])
 
-    def dl_limiter(n_articles=5):
-        """Limits the downloaded articles to test.""""
+    # def dl_limiter(n_articles=5):
+    #     """Limits the downloaded articles to test.""""
 
-    def test__get_articles(self):
-        """Test get_articles method.""""
+    # def test__get_articles(self):
+    #     """Test get_articles method.""""
 
-        stage = ''
-        f = open('tests/stage.json')
-
+    #     stage = ''
+    #     f = open('tests/stage.json')
+    #     self.assertListEqual()
 
 if __name__ == '__main__':
     unittest.main()
